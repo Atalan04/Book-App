b82ac067dde234343f498a6caa4b14d3f92d5800
Ahmadreza Hasanein <ahmadreza.hasanein@gmail.com>
2026-06-07 17:33:26 +0400

    add book list as a main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Layout from "./layout/Layout.jsx"
+import BookList from "./components/BookList.jsx";
+import Layout from "./layout/Layout.jsx";
 
 function App() {
   return (
     <Layout>
-      
+      <BookList />
     </Layout>
   );
 }
