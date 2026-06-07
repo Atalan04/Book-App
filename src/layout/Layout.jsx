051c2b5a93edbe6979c6d0213cb77fdcd105e5f6
Ahmadreza Hasanein <ahmadreza.hasanein@gmail.com>
2026-06-07 17:33:56 +0400

    use children props

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,10 @@
-function Layout() {
+function Layout({ children }) {
   return (
     <>
       <header>
         <h1>Book App</h1>
       </header>
+      {children}
       <footer>
         <h3>Developed by Ahmadreza with ❤️</h3>
       </footer>
