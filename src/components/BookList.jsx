31d1786d43b0f5cd7efcee21d11a68705cb0b219
Ahmadreza Hasanein <ahmadreza.hasanein@gmail.com>
2026-06-07 18:45:28 +0400

    show book list, with map, set favorite button and eventHandler, show favorite list with pushing like button

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,2 +1,35 @@
-import  mockData from "./constants/mockData.js"
+import { useState } from "react";
+import { books } from "../constants/mockData.js";
+import FavoriteList from "./FavoriteList.jsx";
 
+function BookList() {
+  const [favorite, setFavorite] = useState([]);
+
+  const handleFavorite = (book) => {
+    const isFavorite = favorite.some((i) => i.id === book.id);
+
+    if (isFavorite) {
+      setFavorite((list) => list.filter((i) => i.id != book.id));
+    } else {
+      setFavorite((list) => [...list, book]);
+    }
+  };
+  return (
+    <>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <img src={book.image} alt={book.title} />-{book.title}-{book.author}
+            -{book.country}-{book.pages}
+            <button onClick={() => handleFavorite(book)}>🤍</button>
+          </li>
+        ))}
+      </ul>
+    
+        <FavoriteList favorite={favorite}/>
+      
+    </>
+  );
+}
+
+export default BookList;
